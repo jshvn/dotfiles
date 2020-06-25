333c79caf9e5cf937ba25c446208643671d0748f
Josh Vaughen <vaughen@adobe.com>
2020-06-25 14:04:21 -0700

    updating macos script to check if xcode tools already installed

--- macos/macos.sh
+++ macos/macos.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 
-# Install Xcode Command Line Tools
-sudo xcode-select --install
-# Accept Xcode license
-sudo xcodebuild -license accept
+if xcode-select -p 1>/dev/null; then
+  echo "Xcode Command Line Tools already installed, skipping installation"
+else
+  echo "Installing Xcode Command Line Tools"
+  # Install Xcode Command Line Tools
+  sudo xcode-select --install
+  # Accept Xcode license
+  sudo xcodebuild -license accept
+fi
 
-source ./defaults.sh
+source defaults.sh
