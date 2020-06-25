4ec4dca70e90934d6a689077892023cbce4173f6
Josh Vaughen <vaughen@adobe.com>
2020-06-25 11:29:23 -0700

    adding main install.sh

---
+++ install.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+set -e
+
+# Set up symbolic links for ZSH and Git pointing to this cloned repo
+ln -sf "$HOME"/Git/personal/dotfiles/zsh/.zshrc "$HOME"/.zshrc
+ln -sf "$HOME"/Git/personal/dotfiles/git/.gitconfig "$HOME"/.gitconfig
+ln -sf "$HOME"/Git/personal/dotfiles/git/.gitignore_global "$HOME"/.gitignore_global
+ln -sf "$HOME"/Git/personal/dotfiles/ssh/.ssh/config "$HOME"/.ssh/config
+
+# Get Oh My ZSH up and running
+if [ ! -e ~/.oh-my-zsh ]; then
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+fi
+
+# Make ZSH the default shell environment (maybe unnecessary on Catalina?)
+# shellcheck disable=SC2230
+chsh -s "$(which zsh)"
+
+if [ "$(uname)" == "Darwin" ]; then
+  # shellcheck disable=SC1091
+  source ./macos/macos.sh
+else
+  echo ""
+  echo "This isn't a Mac, so we're all done here!"
+  echo "Logout/restart now for the full effects."
+  exit 0
+fi
