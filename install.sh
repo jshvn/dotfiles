533538fd855321dcceba165490f28285432f40a0
Josh Vaughen <vaughen@adobe.com>
2020-06-25 15:37:47 -0700

    fixing directory issues, making them agnostic of repo location

--- install.sh
+++ install.sh
@@ -2,13 +2,22 @@
 
 set -e
 
+# find git directory
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+GITDIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+
 # Set up symbolic links for ZSH and Git pointing to this cloned repo
-ln -sf "$HOME"/Git/personal/dotfiles/zsh/.zshrc "$HOME"/.zshrc
-ln -sf "$HOME"/Git/personal/dotfiles/zsh/aliases.zsh "$HOME"/.aliases.zsh
-ln -sf "$HOME"/Git/personal/dotfiles/zsh/functions.zsh "$HOME"/.functions.zsh
-ln -sf "$HOME"/Git/personal/dotfiles/git/.gitconfig "$HOME"/.gitconfig
-ln -sf "$HOME"/Git/personal/dotfiles/git/.gitignore_global "$HOME"/.gitignore_global
-ln -sf "$HOME"/Git/personal/dotfiles/ssh/.ssh/config "$HOME"/.ssh/config
+ln -sf "$GITDIR"/zsh/.zshrc "$HOME"/.zshrc
+ln -sf "$GITDIR"zsh/aliases.zsh "$HOME"/.aliases.zsh
+ln -sf "$GITDIR"/functions.zsh "$HOME"/.functions.zsh
+ln -sf "$GITDIR"/git/.gitconfig "$HOME"/.gitconfig
+ln -sf "$GITDIR"/git/.gitignore_global "$HOME"/.gitignore_global
+ln -sf "$GITDIR"/ssh/.ssh/config "$HOME"/.ssh/config
 
 
 # Get Oh My ZSH up and running
@@ -17,7 +26,7 @@ if [ ! -e ~/.oh-my-zsh ]; then
 fi
 
 if [ "$(uname)" == "Darwin" ]; then
-  source ./macos/macos.sh
+  source "$GITDIR"/macos/macos.sh
 else
   echo ""
   echo "This isn't a Mac, so we're all done here!"
--- macos/macos.sh
+++ macos/macos.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 
+# find git directory
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+GITDIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+
 if xcode-select -p 1>/dev/null; then
   echo "Xcode Command Line Tools already installed, skipping installation"
 else
@@ -10,4 +19,4 @@ else
   sudo xcodebuild -license accept
 fi
 
-source defaults.sh
+source "$GITDIR"/defaults.sh
