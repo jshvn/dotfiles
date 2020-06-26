c305546118cc6ec565021c1b1b7aeccc42227e89
Josh Vaughen <vaughen@adobe.com>
2020-06-26 10:33:49 -0700

    adding initial cheatsheet infrastructure

---
+++ cheat/conda.md
@@ -0,0 +1,20 @@
+### Conda basics
+
+|Task|Command|
+|:--|:--|
+|Verify conda is installed, check version number|`conda info`|
+|Update conda to the current version|`conda update conda`|
+
+### Using environments
+
+
+### finding conda packages
+
+### Installing and updating packages
+
+### Managing multiple versions of Python
+
+
+### Specifying version numbers
+
+
---
+++ zsh/scripts/cheat.sh
@@ -0,0 +1,19 @@
+# set cheatsdir to the directory where cheat markdown files are located
+SOURCE="${(%):-%N}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+    DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+    SOURCE="$(readlink "$SOURCE")"
+    [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+GITDIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+parentdir="$(dirname "$GITDIR")"
+cheatsdir="$(dirname "$parentdir")"/cheat/
+ 
+
+function cheat() {
+    case $1 in
+        conda)   glow "$cheatsdir/conda.md"    ;;
+        help)    echo "blah"     ;;
+        *)       echo "'$1' wasn't found in list of cheat sheets" ;;
+    esac
+}
