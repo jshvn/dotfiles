f0ab856aa689bdb359110a0a88ca63fbdf6bd8c7
Josh Vaughen <vaughen@adobe.com>
2020-06-26 10:33:33 -0700

    update zsh prompt theme

--- zsh/theme.zsh
+++ zsh/theme.zsh
@@ -12,5 +12,5 @@ LS_COLORS='rs=0:di=01;34:ln=01;36:mh=00:pi=40;33:so=01;35:do=01;35:bd=40;33;01:c
 export LS_COLORS
 
 # set custom prompt colors for the "alanpeabody" theme
-PROMPT='%{$fg[green]%}%n@%{$fg[green]%}%m%{$reset_color%} %{$fg[blue]%}%~%{$reset_color%}$ '
+PROMPT='%{$fg[green]%}%n@%{$fg[green]%}%m%{$reset_color%}:%{$fg[blue]%}%~%{$reset_color%}$ '
 export PROMPT
