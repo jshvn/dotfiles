e91b7b713218c8e1c2d0f3f37ef63fff1dffa691
Josh Vaughen <vaughen@adobe.com>
2020-06-26 13:20:22 -0700

    updating cheat documentation

--- zsh/scripts/cheat.sh
+++ zsh/scripts/cheat.sh
@@ -13,8 +13,9 @@ cheatsdir="$rootdir"/cheat/
 
 cheathelp="""
 Available cheat functions:
-    $ cheat reload      :   reloads zsh configuration
-    $ cheat conda [pdf] :   lists useful conda functions and operations. append 'pdf' to open pdf copy    
+    $ reload            :   reloads zsh configuration
+    $ cheat conda [pdf] :   lists useful conda functions and operations. append 'pdf' to open pdf copy 
+    $ cheat git [pdf]   :   lists useful git commands. append 'pdf' to open pdf copy   
     $ cheat zsh         :   lists useful zsh tips and tricks
     $ cheat aliases     :   lists all of the custom aliases defined in dotfiles
     $ cheat functions   :   lists all of the custom fucntions defined in dotfiles
