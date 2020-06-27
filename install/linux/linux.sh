039234de3fb353ae646c66c529bb15834dc0c244
Josh Vaughen <vaughen@adobe.com>
2020-06-27 14:23:19 -0700

    adding bin bash prefix to test a theory

--- install/linux/linux.sh
+++ install/linux/linux.sh
@@ -1,27 +1,27 @@
 #!/usr/bin/env bash
 
-echo "Installing homebrew..."
+/bin/bash echo "Installing homebrew..."
 if test ! "$(which brew)"; then
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 fi
 
 # now, because homebrew installs to /home/linuxbrew/.linuxbrew/ by default it obviously won't be in my path 
 # so I need to go ahead and update my path and profile
-echo 'eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)' >> ~/.profile
+/bin/bash echo 'eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)' >> ~/.profile
 #eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
 
 # Update Homebrew recipes
 echo "Updating homebrew..."
-brew update
+/bin/bash brew update
 
 # gcc is needed by a number of binaries, go ahead and install it
-brew install gcc
+/bin/bash brew install gcc
 
 # Install all apps from the Brewfile
-echo "Installing all packages and applications from the Brewfile"
-brew tap homebrew/bundle
-brew bundle --file "$DOTFILEDIR"/install/common/Brewfile
-brew bundle --file "$DOTFILEDIR"/install/linux/Brewfile
+/bin/bash echo "Installing all packages and applications from the Brewfile"
+/bin/bash brew tap homebrew/bundle
+/bin/bash brew bundle --file "$DOTFILEDIR"/install/common/Brewfile
+/bin/bash brew bundle --file "$DOTFILEDIR"/install/linux/Brewfile
 
 # install complete
-echo "Install complete! You'll probably need to log back in to switch to ZSH, or simply run $ chsh -s $(which zsh)"
+/bin/bash echo "Install complete! You'll probably need to log back in to switch to ZSH, or simply run $ chsh -s $(which zsh)"
