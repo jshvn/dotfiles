4022d2c785968287bf112517536c6e732db497c6
Josh Vaughen <vaughen@adobe.com>
2020-06-27 19:08:30 -0700

    add dotfiles to alias

--- zsh/common/aliases.zsh
+++ zsh/common/aliases.zsh
@@ -7,3 +7,7 @@ alias reload="source ~/.zshrc"
 
 # Print each PATH entry on a separate line
 alias path='echo -e ${PATH//:/\\n}'
+
+# print the dotfile directory
+alias dotfile='echo $DOTFILEDIR'
+alias dotfiles='echo $DOTFILEDIR'
