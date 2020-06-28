570de134fd3c9b7643e4574ee1ee5e6226884a56
Josh Vaughen <vaughen@adobe.com>
2020-06-27 20:23:22 -0700

    update dotfiles install prints

--- install/install.sh
+++ install/install.sh
@@ -105,7 +105,8 @@ You might also need to manually set your shell if the settings didn't take. To
 do that you can run the command: $ chsh -s $(which zsh)
 
 If you want to update to the latest version of the dotfiles, run the following commands:
-    $ dotfile 
+    $ dotfiles
     $ git pull
     $ ./bootstrap.sh
+    
 """
