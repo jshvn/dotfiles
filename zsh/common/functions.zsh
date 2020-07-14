a2870896dcbe46237442f2dfc5a052613c6090cf
Josh Vaughen <josh@vaughen.net>
2020-07-13 21:46:24 -0700

    refactoring helpful scripts

--- zsh/common/functions.zsh
+++ zsh/common/functions.zsh
@@ -66,38 +66,6 @@ function fs() {    # fs() will print a human readable size of given file or dire
 	fi;
 }
 
-# Grabs certificates for a given domain name
-function getcertnames() {    # getcertnames() will print SSL certificate information for a given domain. ex: $ getcertnames ijosh.com
-	if [ -z "${1}" ]; then
-		echo "ERROR: No domain specified.";
-		return 1;
-	fi;
-
-	local domain="${1}";
-	echo "Testing ${domain}…";
-	echo ""; # newline
-
-	local tmp=$(echo -e "GET / HTTP/1.0\nEOT" \
-		| openssl s_client -connect "${domain}:443" -servername "${domain}" 2>&1);
-
-	if [[ "${tmp}" = *"-----BEGIN CERTIFICATE-----"* ]]; then
-		local certText=$(echo "${tmp}" \
-			| openssl x509 -text -certopt "no_aux, no_header, no_issuer, no_pubkey, \
-			no_serial, no_sigdump, no_signame, no_validity, no_version");
-		echo "Common Name:";
-		echo ""; # newline
-		echo "${certText}" | grep "Subject:" | sed -e "s/^.*CN=//" | sed -e "s/\/emailAddress=.*//";
-		echo ""; # newline
-		echo "Subject Alternative Name(s):";
-		echo ""; # newline
-		echo "${certText}" | grep -A 1 "Subject Alternative Name:" \
-			| sed -e "2s/DNS://g" -e "s/ //g" | tr "," "\n" | tail -n +2;
-		return 0;
-	else
-		echo "ERROR: Certificate not found.";
-		return 1;
-	fi;
-}
 
 # Prints permissions of file
 function permissions() {    # permissions() will print human readable permissions for a given file or directory. ex: $ permissions ~
@@ -113,14 +81,3 @@ function permissions() {    # permissions() will print human readable permission
     fi;
 }
 
-
-# Geo IP lookup helper, uses # https://freegeoip.app/ as source
-function geoip() {
-		if [ -z "${1}" ]; then
-		echo "ERROR: No IP or host specified";
-		return 1;
-	fi;
-
-	jsonobject=$(curl -s --request GET --url https://freegeoip.app/json/$1 --header 'accept: application/json' --header 'content-type: application/json' | python -m json.tool)
-	echo $jsonobject | highlight --syntax=json --out-format=xterm256
-}
---
+++ zsh/common/scripts/geoip.zsh
@@ -0,0 +1,10 @@
+# Geo IP lookup helper, uses # https://freegeoip.app/ as source
+function geoip() {    # geoip() will print geolocation information for a given IP or host. ex: $ geoip 1.1.1.1
+		if [ -z "${1}" ]; then
+		echo "ERROR: No IP or host specified";
+		return 1;
+	fi;
+
+	jsonobject=$(curl -s --request GET --url https://freegeoip.app/json/$1 --header 'accept: application/json' --header 'content-type: application/json' | python -m json.tool)
+	echo $jsonobject | highlight --syntax=json --out-format=xterm256
+}
