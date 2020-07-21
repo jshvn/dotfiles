57fcc9009a6457a9508cfcb4bb17d5dca94e1172
Josh Vaughen <josh@vaughen.net>
2020-07-21 09:55:21 -0700

    add pretty json, eventually refactor this to handle arbitrary file types

--- zsh/common/functions.zsh
+++ zsh/common/functions.zsh
@@ -86,3 +86,14 @@ function permissions() {    # permissions() will print human readable permission
     fi;
 }
 
+
+# pretty print json
+function prettyjson() {    # prettyjson() will print human readable json that has been colorized. ex: $ prettyjson file.json
+	if [ -z "${1}" ]; then
+		echo "ERROR: No file specified";
+		return 1;
+	fi;
+
+  result=$(cat $1 | python -m json.tool)
+  echo $result | highlight --syntax=json
+}
