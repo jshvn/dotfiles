1167011f10d76a38132e26b97882befd08a0cc8f
Josh Vaughen <josh@vaughen.net>
2020-07-31 16:37:46 -0700

    updating geoip provider

--- zsh/common/scripts/geoip.zsh
+++ zsh/common/scripts/geoip.zsh
@@ -1,10 +1,10 @@
-# Geo IP lookup helper, uses # https://freegeoip.app/ as source
+# Geo IP lookup helper, uses # https://ipinfo.io/ as source
 function geoip() {    # geoip() will print geolocation information for a given IP or host. ex: $ geoip 1.1.1.1
 		if [ -z "${1}" ]; then
 		echo "ERROR: No IP or host specified";
 		return 1;
 	fi;
 
-	jsonobject=$(curl -s --request GET --url https://freegeoip.app/json/$1 --header 'accept: application/json' --header 'content-type: application/json' | python -m json.tool)
+	jsonobject=$(curl -s --request GET --url ipinfo.io/$1 --header 'accept: application/json' --header 'content-type: application/json' | python -m json.tool)
 	echo $jsonobject | highlight --syntax=json
 }
