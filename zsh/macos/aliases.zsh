e46d6703c9f7fcdeb2ed38bebefc99ec168bb8ed
Josh Vaughen <josh@vaughen.net>
2021-01-11 13:16:20 -0800

    update afk alias for big sur

--- zsh/macos/aliases.zsh
+++ zsh/macos/aliases.zsh
@@ -10,9 +10,6 @@ alias ls="/usr/local/bin/gls --color=always"
 # open finder at current location
 alias finder="open -a Finder ./"
 
-# going afk
-alias afk="/System/Library/CoreServices/Menu\ Extras/User.menu/Contents/Resources/CGSession -suspend"
-
 # Show/hide hidden files in Finder
 alias findershow="defaults write com.apple.finder AppleShowAllFiles -bool true && killall Finder"
 alias finderhide="defaults write com.apple.finder AppleShowAllFiles -bool false && killall Finder"
--- zsh/macos/functions.zsh
+++ zsh/macos/functions.zsh
@@ -36,3 +36,15 @@ function sethostname() {    # sethostname() will set the machine's hostname to t
 	sudo scutil --set LocalHostName $1
 	sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName -string $1
 }
+
+# afk function
+function afk() {	# afk() will suspend the current session and lock the screen. ex: $ afk
+	autoload is-at-least
+	if is-at-least 11.0 $(sw_vers -productVersion); then
+		# big sur or greater
+  		osascript -e 'tell app "System Events" to key code 12 using {control down, command down}'
+	else
+		# catalina or lower
+		/System/Library/CoreServices/Menu\ Extras/User.menu/Contents/Resources/CGSession -suspend
+	fi
+}
