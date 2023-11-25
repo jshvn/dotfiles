51326b89890463408f07a9b7f61999ad7ff83f37
Josh Vaughen <josh@vaughen.net>
2023-11-24 18:31:31 -0800

    updating geoip lookup source

--- zsh/scripts/geoip.zsh
+++ zsh/scripts/geoip.zsh
@@ -1,10 +1,21 @@
 # Geo IP lookup helper, uses # https://ipinfo.io/ as source
+# function geoip() {    # geoip() will print geolocation information for a given IP or host. ex: $ geoip 1.1.1.1
+# 	if [ -z "${1}" ]; then
+# 		echo "ERROR: No IP or host specified";
+# 		return 1;
+# 	fi;
+
+# 	local jsonobject=$(curl -s --request GET --url ipinfo.io/$1 --header 'accept: application/json' --header 'content-type: application/json' | python -m json.tool)
+# 	echo $jsonobject | highlight --syntax=json
+# }
+
+# Geo IP lookup helper, uses # https://ipinfo.io/ as source
 function geoip() {    # geoip() will print geolocation information for a given IP or host. ex: $ geoip 1.1.1.1
-		if [ -z "${1}" ]; then
+    if [ -z "${1}" ]; then
 		echo "ERROR: No IP or host specified";
 		return 1;
 	fi;
 
-	jsonobject=$(curl -s --request GET --url ipinfo.io/$1 --header 'accept: application/json' --header 'content-type: application/json' | python -m json.tool)
-	echo $jsonobject | highlight --syntax=json
+    local jsonobject=$(curl -sL --request GET --url ip.guide/$1 --header 'accept: application/json' --header 'content-type: application/json' | python -m json.tool)
+    echo $jsonobject | highlight --syntax=json
 }
