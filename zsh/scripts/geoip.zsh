a23e33765e7f6ae1a8808357ca8644bc6785f8c3
Josh Vaughen <josh@vaughen.net>
2023-11-30 16:54:28 -0800

    remove old geoip func

--- zsh/scripts/geoip.zsh
+++ zsh/scripts/geoip.zsh
@@ -1,15 +1,4 @@
-# Geo IP lookup helper, uses # https://ipinfo.io/ as source
-# function geoip() {    # geoip() will print geolocation information for a given IP or host. ex: $ geoip 1.1.1.1
-# 	if [ -z "${1}" ]; then
-# 		echo "ERROR: No IP or host specified";
-# 		return 1;
-# 	fi;
-
-# 	local jsonobject=$(curl -s --request GET --url ipinfo.io/$1 --header 'accept: application/json' --header 'content-type: application/json' | python -m json.tool)
-# 	echo $jsonobject | highlight --syntax=json
-# }
-
-# Geo IP lookup helper, uses # https://ipinfo.io/ as source
+# Geo IP lookup helper, uses # https://ip.guide/ as source
 function geoip() {    # geoip() will print geolocation information for a given IP or host. ex: $ geoip 1.1.1.1
     if [ -z "${1}" ]; then
 		echo "ERROR: No IP or host specified";
