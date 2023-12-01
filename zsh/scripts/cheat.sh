56483d93c1fde478cac192d0c9bccbabbc3d4a26
Josh Vaughen <josh@vaughen.net>
2023-11-30 17:52:47 -0800

    moving cheat into its own function file

---
+++ zsh/scripts/cheat.sh
@@ -0,0 +1,12 @@
+# Print out helpful cheat pages to understand how commands and utilities work
+function cheat() {    # cheat() will query a web service and return a human-readable man page for that command. ex $ cheat ls
+	if [ -z "${1}" ]; then
+		echo "ERROR: No command specified. Printing help instead.";
+        result=$(curl "cheat.sh" -s)
+        echo $result
+		return 1;
+	fi;
+    
+    result=$(curl "cheat.sh/${1}?style=xcode" -s)
+    echo $result
+}
