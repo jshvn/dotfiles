5daebfa40bdbe0adcaaa42de9b5c70daef96391f
Josh Vaughen <josh@vaughen.net>
2023-12-21 12:33:46 -0800

    add info script

---
+++ zsh/scripts/info.zsh
@@ -0,0 +1,8 @@
+# Print information related to current system or git repository
+function info() {    # info() will print git repository info if inside a repository, otherwise print system info. ex: $ info
+    git check-ignore -q . 2>/dev/null; if [ "$?" -ne "1" ]; then
+        neofetch
+    else
+        onefetch
+    fi;
+}
