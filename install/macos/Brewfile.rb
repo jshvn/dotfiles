9560fcf7dff50b206f56d18a021595fef1bc2295
Josh Vaughen <josh@vaughen.net>
2025-10-19 21:51:37 -0700

    allow casks to be upgraded by brew

--- install/macos/Brewfile.rb
+++ install/macos/Brewfile.rb
@@ -20,40 +20,40 @@ brew "mas" # https://github.com/mas-cli/mas
 cask_args appdir: "/Applications"
 
 # social
-cask "discord" unless system("test -e /Applications/Discord.app")
-cask "slack" unless system("test -e /Applications/Slack.app")
+cask "discord"
+cask "slack"
 
 # music 
-cask "spotify" unless system("test -e /Applications/Spotify.app")
+cask "spotify"
 
 # development
-cask "sourcetree" unless system("test -e /Applications/Sourcetree.app")
-cask "sublime-text" unless system("test -e \"/Applications/Sublime Text.app\"")
-cask "visual-studio-code" unless system("test -e \"/Applications/Visual Studio Code.app\"")
+cask "sourcetree"
+cask "sublime-text"
+cask "visual-studio-code"
 
 # utilities
-cask "appcleaner" unless system("test -e /Applications/AppCleaner.app")
-cask "protonvpn" unless system("test -e /Applications/ProtonVPN.app")
-cask "proton-mail" unless system("test -e \"/Applications/Proton Mail.app\"")
-cask "proton-drive" unless system("test -e \"/Applications/Proton Drive.app\"")
-cask "dropbox" unless system("test -e /Applications/Dropbox.app")
-cask "1password" unless system("test -e /Applications/1Password.app")
-cask "cryptomator" unless system("test -e /Applications/Cryptomator.app")
-cask "raycast" unless system("test -e /Applications/Raycast.app")
+cask "appcleaner"
+cask "protonvpn"
+cask "proton-mail"
+cask "proton-drive"
+cask "dropbox"
+cask "1password"
+cask "cryptomator"
+cask "raycast"
 
 # productivity
-cask "standard-notes" unless system("test -e \"/Applications/Standard Notes.app\"")
-cask "fantastical" unless system("test -e /Applications/Fantastical.app")
-cask "cardhop" unless system("test -e /Applications/Cardhop.app")
-cask "microsoft-word" unless system("test -e \"/Applications/Microsoft Word.app\"")
-cask "microsoft-excel" unless system("test -e \"/Applications/Microsoft Excel.app\"")
-cask "microsoft-powerpoint" unless system("test -e \"/Applications/Microsoft PowerPoint.app\"")
+cask "standard-notes"
+cask "fantastical"
+cask "cardhop"
+cask "microsoft-word"
+cask "microsoft-excel"
+cask "microsoft-powerpoint"
 
 # games
-cask "nvidia-geforce-now" unless system("test -e /Applications/GeForceNOW.app")
+cask "nvidia-geforce-now"
 
 # browser
-cask "firefox" unless system("test -e /Applications/Firefox.app")
+cask "firefox"
 
 ###############################
 #  macOS Utilities via Cask   #
