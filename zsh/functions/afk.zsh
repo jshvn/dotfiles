4e9b39919b6a73b10dcb31531063ca4f0ef848c6
Josh Vaughen <josh@vaughen.net>
2025-11-08 23:32:19 -0800

    fix afk function to work on tahoe

--- zsh/functions/afk.zsh
+++ zsh/functions/afk.zsh
@@ -2,12 +2,5 @@
 
 # afk func
 function afk() {	# afk() will suspend the current session and lock the screen. ex: $ afk
-    autoload is-at-least
-    if is-at-least 11.0 $(sw_vers -productVersion); then
-        # big sur or greater
-        osascript -e 'tell app "System Events" to key code 12 using {control down, command down}'
-    else
-        # catalina or lower
-        /System/Library/CoreServices/Menu\ Extras/User.menu/Contents/Resources/CGSession -suspend
-    fi
+    pmset displaysleepnow
 }
