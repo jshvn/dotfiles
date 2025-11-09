a00ea69b1af06f48c0e44a2e80a0f0c9598ca6b7
Josh Vaughen <josh@vaughen.net>
2025-11-08 23:32:32 -0800

    fix format of timezsh function to comply with standard

--- zsh/functions/timezsh.zsh
+++ zsh/functions/timezsh.zsh
@@ -1,6 +1,7 @@
 #!/bin/zsh
 
-timezsh() {    # timezsh() will time how long it takes to start an interactive zsh shell. ex: $ timezsh
+# profile zsh startup time
+function timezsh() {    # timezsh() will time how long it takes to start an interactive zsh shell. ex: $ timezsh
   shell=${1-$SHELL}
   for i in $(seq 1 4); do /usr/bin/time $shell -i -c exit; done
 }
