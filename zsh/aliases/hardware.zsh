4011fbc29f8446b2bd6d50c939cc51ba8b2c1583
Josh Vaughen <josh@vaughen.net>
2025-11-12 11:36:17 -0800

    update hardware alias definitions, add new aliases

--- zsh/aliases/hardware.zsh
+++ zsh/aliases/hardware.zsh
@@ -1,4 +1,11 @@
 #!/bin/zsh
 
-alias gpu="system_profiler SPDisplaysDataType"
-alias cpu="sysctl -n machdep.cpu.brand_string"
+alias gpu="system_profiler SPDisplaysDataType | highlight --syntax=markdown"
+alias cpu="sysctl -n machdep.cpu.brand_string | highlight --syntax=markdown"
+alias ram="system_profiler SPMemoryDataType | highlight --syntax=markdown"
+alias disk="diskutil list | highlight --syntax=markdown"
+alias bluetooth="system_profiler SPBluetoothDataType | highlight --syntax=markdown"
+alias speaker="system_profiler SPAudioDataType | highlight --syntax=markdown"
+alias webcam="system_profiler SPCameraDataType | highlight --syntax=markdown"
+alias power="system_profiler SPPowerDataType | highlight --syntax=markdown"
+alias monitor="system_profiler SPDisplaysDataType | highlight --syntax=markdown"
