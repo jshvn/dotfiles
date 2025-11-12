12d1ecb06cdb80f2bf18b11c2af536a81eb207ae
Josh Vaughen <josh@vaughen.net>
2025-11-12 11:01:28 -0800

    modify comment such that it does not get caught in functionlist

--- zsh/functions/update.zsh
+++ zsh/functions/update.zsh
@@ -15,7 +15,7 @@ function update() {    # update() will update the current dotfiles installation 
 	# Run the updater in a child zsh so any `exec` performed by the updater
 	# replaces only the child process — not the current interactive shell
 	# (when `omz update` applies an update it may exec a new shell, which
-	# would otherwise replace the shell running this function and terminate
+	# would otherwise replace the shell running this func and terminate
 	# the rest of the script).
 	zsh -ic 'omz update'
 
