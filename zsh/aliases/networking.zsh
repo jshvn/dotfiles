b7f6d12af128f93ece3c3dfe6e5018fa4ce55421
Josh Vaughen <josh@vaughen.net>
2025-11-12 11:22:23 -0800

    clean up networking aliases

--- zsh/aliases/networking.zsh
+++ zsh/aliases/networking.zsh
@@ -3,15 +3,12 @@
 # flush dns cache
 alias dnsflush="sudo killall -HUP mDNSResponder; sudo killall mDNSResponderHelper; sudo dscacheutil -flushcache"
 
-# get current IP information. show all: $ ips
+# get current active network interfaces
 local activeinterfaces=$(ifconfig | awk '/^[^[:space:]]+:/ { iface=$1; sub(/:$/,"",iface) } /status: active/ { print iface }' | tr '\n' ' ')
-local getiploc=$(ipconfig getifaddr en0)
+alias interfaces="echo Active Interfaces: $activeinterfaces"
 
 # replace traceroute with trip
 alias traceroute="$(which trip) -u"
-
-alias iploc="echo Local IP: $getiploc"
-alias interfaces="echo Active Interfaces: $activeinterfaces"
 
 # use ipv4lookup and ipv6lookup functions to get IPs
 alias ip="ipv4lookup; ipv6lookup;"
