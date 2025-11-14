48d98c7fb8008ba35e2f56ffc371d845600f5d55
Josh Vaughen <josh@vaughen.net>
2025-11-14 12:40:44 -0800

    update timezsh function to better profile startup times

--- zsh/functions/timezsh.zsh
+++ zsh/functions/timezsh.zsh
@@ -2,6 +2,10 @@
 
 # profile zsh startup time
 function timezsh() {    # timezsh() will time how long it takes to start an interactive zsh shell. ex: $ timezsh
-  shell=${1-$SHELL}
-  for i in $(seq 1 4); do /usr/bin/time $shell -i -c exit; done
+    # first run shell startup 4 times with /usr/bin/time to get average time
+    shell=${1-$SHELL}
+    for i in $(seq 1 4); do /usr/bin/time $shell -i -c exit; done
+
+    # now run with zprof to get detailed profiling info
+    $shell -i -c 'zmodload zsh/zprof; source $HOME/.zshrc; zprof'
 }
