cd6ddf0fca54219e31733272395eafc5916285f7
Josh Vaughen <josh@vaughen.net>
2025-12-08 22:14:27 -0800

    bug fix shell command

--- zsh/functions/docker.zsh
+++ zsh/functions/docker.zsh
@@ -6,6 +6,7 @@ function docker() { # docker() will customize the functionality of docker. ex: $
         shift
         command docker ps --format "table {{.Names}}\t{{.Image}}\t{{.Status}}" "$@"
     elif [[ "$1" == "bash" || "$1" == "sh" || "$1" == "csh" || "$1" == "dash" || "$1" == "ksh" || "$1" == "tcsh" || "$1" == "zsh" ]]; then
+        local shell="$1"
         shift
         if [ -z "${1}" ]; then
             echo "ERROR: No container specified"
@@ -13,7 +14,7 @@ function docker() { # docker() will customize the functionality of docker. ex: $
             docker ps --format "table {{.Names}}\t{{.Image}}\t{{.Status}}"
             return 1
         fi
-        command docker exec -ti "$1" "/bin/$1"
+        command docker exec -ti "$1" "/bin/$shell"
     else
         command docker "$@"
     fi
