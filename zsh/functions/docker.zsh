8803f05a964fb70ea69d5d3c7d7c978ddadeb619
Josh Vaughen <josh@vaughen.net>
2025-12-08 22:12:29 -0800

    update docker function

--- zsh/functions/docker.zsh
+++ zsh/functions/docker.zsh
@@ -5,7 +5,7 @@ function docker() { # docker() will customize the functionality of docker. ex: $
     if [[ "$1" == "ps" ]]; then
         shift
         command docker ps --format "table {{.Names}}\t{{.Image}}\t{{.Status}}" "$@"
-    elif [[ "$1" == "bash" || "$1" == "sh" || "$1" == "shell" ]]; then
+    elif [[ "$1" == "bash" || "$1" == "sh" || "$1" == "csh" || "$1" == "dash" || "$1" == "ksh" || "$1" == "tcsh" || "$1" == "zsh" ]]; then
         shift
         if [ -z "${1}" ]; then
             echo "ERROR: No container specified"
@@ -13,7 +13,7 @@ function docker() { # docker() will customize the functionality of docker. ex: $
             docker ps --format "table {{.Names}}\t{{.Image}}\t{{.Status}}"
             return 1
         fi
-        command docker exec -ti "$1" /bin/bash
+        command docker exec -ti "$1" "/bin/$1"
     else
         command docker "$@"
     fi
